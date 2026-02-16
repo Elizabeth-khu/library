6b344fe4285f45bad8e6e2f50e1239e74a9e3321
elizabeth <elizahudoley@gmail.com>
2026-02-16 13:04:06 +0300

    Localize empty book list output

--- src/main/java/com/example/library/ui/command/DisplayBooksCommand.java
+++ src/main/java/com/example/library/ui/command/DisplayBooksCommand.java
@@ -1,5 +1,6 @@
 package com.example.library.ui.command;
 
+import com.example.library.i18n.Translator;
 import com.example.library.storage.BooksStorage;
 import com.example.library.ui.BookFormatter;
 import com.example.library.ui.ConsoleIO;
@@ -15,16 +16,25 @@ public class DisplayBooksCommand implements Command {
     private final BooksStorage booksStorage;
     private final BookFormatter bookFormatter;
     private final ConsoleIO consoleIO;
+    private final Translator translator;
 
-    public DisplayBooksCommand(BooksStorage booksStorage, BookFormatter bookFormatter, ConsoleIO consoleIO) {
+    public DisplayBooksCommand(BooksStorage booksStorage, BookFormatter bookFormatter, ConsoleIO consoleIO, Translator translator) {
         this.booksStorage = booksStorage;
         this.bookFormatter = bookFormatter;
         this.consoleIO = consoleIO;
+        this.translator = translator;
     }
 
     @Override
     public void execute() {
         log.info("Displaying book list");
-        consoleIO.println(bookFormatter.formatList(booksStorage.books()));
+        var books = booksStorage.books();
+
+        if (books.isEmpty()) {
+            consoleIO.println(translator.translate("books.none"));
+            return;
+        }
+
+        consoleIO.println(bookFormatter.formatList(books));
     }
 }

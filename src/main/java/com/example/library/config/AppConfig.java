3080d48ded06ebd1aaf8ea3ceb75c19a883f59bd
elizabeth <elizahudoley@gmail.com>
2026-03-03 12:11:23 +0300

    Import DbConfig and configure DB properties

--- src/main/java/com/example/library/config/AppConfig.java
+++ src/main/java/com/example/library/config/AppConfig.java
@@ -3,9 +3,11 @@ package com.example.library.config;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.context.annotation.Import;
 
 @Configuration
 @ComponentScan("com.example.library")
 @EnableAspectJAutoProxy
+@Import(DbConfig.class)
 public class AppConfig {
 }

40a42699817cfdc8e597da97fa9c0585f1cfff1d
elizabeth <elizahudoley@gmail.com>
2026-03-10 13:04:48 +0300

    Remove redundant DbConfig import from AppConfig

--- src/main/java/com/example/library/config/AppConfig.java
+++ src/main/java/com/example/library/config/AppConfig.java
@@ -3,11 +3,9 @@ package com.example.library.config;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
-import org.springframework.context.annotation.Import;
 
 @Configuration
 @ComponentScan("com.example.library")
 @EnableAspectJAutoProxy
-@Import(DbConfig.class)
 public class AppConfig {
 }

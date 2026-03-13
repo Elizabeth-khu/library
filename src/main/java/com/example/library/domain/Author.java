8314692729c6e57ce62378337064bd309a916e83
elizabeth <elizahudoley@gmail.com>
2026-03-13 18:10:24 +0300

    Map Book and Author as Hibernate entities

--- src/main/java/com/example/library/domain/Author.java
+++ src/main/java/com/example/library/domain/Author.java
@@ -1,3 +1,61 @@
 package com.example.library.domain;
 
-public record Author(long id, String name) { }
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+@Entity
+@Table(name = "authors")
+public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "name", nullable = false, unique = true)
+    private String name;
+
+    @ManyToMany(mappedBy = "authors")
+    private Set<Book> books = new HashSet<>();
+
+    public Author() {
+    }
+
+    public Author(String name) {
+        this.name = name;
+    }
+
+    public Author(long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Set<Book> getBooks() {
+        return books;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Author author)) return false;
+        return id == author.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
--- src/main/java/com/example/library/domain/Book.java
+++ src/main/java/com/example/library/domain/Book.java
@@ -1,13 +1,37 @@
 package com.example.library.domain;
 
+import javax.persistence.*;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
+@Entity
+@Table(name = "books")
 public class Book {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
+    @Column(name = "title", nullable = false)
     private String title;
-    private String author;
+
+    @Column(name = "description")
     private String description;
+
+    @Transient
+    private String author;
+
+    @ManyToMany
+    @JoinTable(
+            name = "book_authors",
+            joinColumns = @JoinColumn(name = "book_id"),
+            inverseJoinColumns = @JoinColumn(name = "author_id")
+    )
+    private Set<Author> authors = new HashSet<>();
+
+    public Book() {
+    }
 
     public Book(long id, String title, String author, String description) {
         this.id = id;
@@ -16,17 +40,52 @@ public class Book {
         this.description = description;
     }
 
-    public long getId() { return id; }
-    public void setId(long id) { this.id = id; }
+    public Book(String title, String description) {
+        this.title = title;
+        this.description = description;
+    }
 
-    public String getTitle() { return title; }
-    public void setTitle(String title) { this.title = title; }
+    public long getId() {
+        return id;
+    }
 
-    public String getAuthor() { return author; }
-    public void setAuthor(String author) { this.author = author; }
+    public String getTitle() {
+        return title;
+    }
 
-    public String getDescription() { return description; }
-    public void setDescription(String description) { this.description = description; }
+    public String getAuthor() {
+        if (author != null) {
+            return author;
+        }
+        return authors.stream()
+                .map(Author::getName)
+                .findFirst()
+                .orElse("");
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Set<Author> getAuthors() {
+        return authors;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void setAuthors(Set<Author> authors) {
+        this.authors = authors;
+    }
 
     @Override
     public boolean equals(Object o) {
